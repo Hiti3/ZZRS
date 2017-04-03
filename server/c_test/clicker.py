21463b0130a6a4f0e257fbff01e89cca276b5d57
kristanm1 <kristanm1@gmail.com>
2017-04-03 23:51:12 +0000

    python-auto-request

---
+++ server/c_test/clicker.py
@@ -0,0 +1,12 @@
+import requests
+import time
+
+
+url = 'https://zzrs-kristanm1.c9users.io/nalozi'
+files = {'file': open('./int_50000x.txt')}
+start = time.time()
+response = requests.post(url, files=files)
+end = time.time()
+
+print(end - start)
+#print(response.text)
