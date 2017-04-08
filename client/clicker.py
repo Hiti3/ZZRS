95c04363f366ff9234509b775a579d99b96e6cca
Miha Bizjak <mihabizjak1@gmail.com>
2017-04-08 14:53:10 +0200

    each client now receives a list of files to sort

--- client/clicker.py
+++ client/clicker.py
@@ -7,23 +7,31 @@ def ask_for_sort(url, file_name):
     start = time.time()
     response = requests.post(url, files=files)
     time_spent = time.time() - start
-    _print([response, time_spent], 0)
     return [response, time_spent]
     
-def _print(tab, print_data):
+def _print(tab, client_id, file_id, print_data):
     num_elements = tab[0].text.split('\n')[0]
-    total_time = round(tab[1], 3)
-    print(num_elements + " elements " + str(total_time) + " s")
+    total_time = str(round(tab[1], 3))
+    print("client: " + client_id + " file: " + file_id + " elements: " + num_elements + " time: " + total_time + " s")
     if(print_data != 0):
         print(tab[0].text)
+    return
+
+def sort_file_list(url, file_list, client_id):
+    for i, file in enumerate(file_list):
+        data = ask_for_sort(url, file)
+        _print(data, str(client_id), str(i), 0)
     return
 
 c9_username = ""
 c9_workspace_name = ""
 url = "https://" + c9_workspace_name + "-" + c9_username + ".c9users.io/nalozi"
-file_name = "inputs/int_10000.txt"
-num_threads = 50
+data_type = "int"
+input_size = 10000
+num_files = 10
+file_list = ["inputs/" + data_type + "_" + str(input_size) + "_" + str(i) + ".txt" for i in range(num_files)]
+num_clients = 10
 
-for i in range(num_threads):
-    t = Thread(target=ask_for_sort, args=(url, file_name))
+for i in range(num_clients):
+    t = Thread(target=sort_file_list, args=(url, file_list, i))
     t.start()
